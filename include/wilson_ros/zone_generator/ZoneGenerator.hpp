9bc44bd868565d1b4937253b42d5b83215aaa884
Arne Hetzenegger <arne.hetzenegger@uni-due.de>
2017-08-20 11:49:14 +0200

    Move ZoneGenRunner code to ZoneGenerator class

--- include/wilson_ros/zone_generator/ZoneGenerator.hpp
+++ include/wilson_ros/zone_generator/ZoneGenerator.hpp
@@ -2,17 +2,32 @@
 // Created by aluquot on 14.08.17.
 //
 
-#ifndef PROJECT_ZONEGENERATOR_HPP
-#define PROJECT_ZONEGENERATOR_HPP
+#ifndef PROJECT_ZONEGENRUNNER_HPP
+#define PROJECT_ZONEGENRUNNER_HPP
 
 #include <ros/ros.h>
 #include <nav_msgs/GridCells.h>
+#include "wilson_ros/zone_generator/ZoneGenerator.hpp"
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseArray.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <wilson_ros/NavigationData.h>
 
 class ZoneGenerator {
 public:
-    ZoneGenerator(double robot_radius, double invalidation_radius) {}
+    ZoneGenerator();
+
+private:
+    ros::NodeHandle nh;
+    ros::Publisher pubZoneCenter;
+    ros::Publisher pubZoneBestMatch;
+    ros::Publisher pubZoneUpperBound;
+    ros::Publisher pubZoneLowerBound;
+    ros::Publisher pubNavigationData;
+    ros::Subscriber sub;
+
     void generateZones(const nav_msgs::GridCells &msg);
 };
 
 
-#endif //PROJECT_ZONEGENERATOR_HPP
+#endif //PROJECT_ZONEGENRUNNER_HPP
--- src/zone_generator/main.cpp
+++ src/zone_generator/main.cpp
@@ -3,11 +3,11 @@
 //
 #include <ros/ros.h>
 #include <nav_msgs/GridCells.h>
-#include <wilson_ros/zone_generator/ZoneGenRunner.hpp>
+#include <wilson_ros/zone_generator/ZoneGenerator.hpp>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "ZoneGenerator");
-    ZoneGenRunner runner;
+    ZoneGenerator runner;
 
     ros::Rate loop_rate(10);
 
